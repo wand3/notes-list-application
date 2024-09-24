b3c4c6f114d5a0e72d82a4cf1b4dd12b15c5ac9d
wand3 <godswillbaiyewu@gmail.com>
2024-09-25 00:02:23 +0100

    Application setup for React, vite, Typescript

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Test from "./components/test";
+
+function App() {
+  return(
+    <Test />
+  );
+
+}
+
+export default App;
---
+++ src/components/test.tsx
@@ -0,0 +1,9 @@
+function Test() {
+    return (
+    <>
+      <h5 className='text-7xl text-center font-bold underline'>Typescript + React + Tailwind</h5>
+    </>
+  );
+}
+
+export default Test;
