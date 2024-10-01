196292d7d553b062760d6c1513f308634978fd53
wand3 <godswillbaiyewu@gmail.com>
2024-10-01 02:01:57 +0100

    notes-list application complete

--- src/templates/ListTemplate.ts
+++ src/templates/ListTemplate.ts
@@ -22,6 +22,7 @@ export default class ListTemplate implements DOMList  {
     clear(): void {
         // clears all the innerhtml of ul 
         this.ul.innerHTML = ''
+        this.ul.className = 'sm:text-sm md:text-xl lg:text-2xl text-[#a1a0b3] dark:text-dark mt-2 bg-transparent'
     }
 
     render(fullList: FullList): void {
@@ -31,11 +32,11 @@ export default class ListTemplate implements DOMList  {
         fullList.list.forEach(item => {
             // create li element and give it a classname 
             const li = document.createElement("li") as HTMLLIElement
-            li.className = "item flex justify-between"
+            li.className = "item dark:border p-[2%] flex w-fill mx-[5%] my-[4%] background-light dark:bg-black-600 dark:drop-shadow-sm rounded-md shadow-sm hover:background-dark-hover focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 
             // create checkbox element and its attributes and append to parent li
             const check = document.createElement("input") as HTMLInputElement
-            check.className = "peer h-auto w-[20px] appearance-none rounded-sm border border-slate-300 font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 checked:appearance-auto"
+            check.className = "peer h-[1em] w-[1em] appearance-none rounded-sm border font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 checked:appearance-auto my-auto mx-2"
             check.type = "checkbox"
             check.id = item.id
             check.tabIndex = 0
@@ -51,7 +52,7 @@ export default class ListTemplate implements DOMList  {
 
             // create the label Element
             const label = document.createElement("label") as HTMLLabelElement
-            label.className = "select-none text-slate-700 peer-checked:text-slate-400 peer-checked:line-through"
+            label.className = "truncate text-wrap select-none peer-checked:text-slate-400 peer-checked:line-through pl-5 w-full"
             label.htmlFor = item.id
             label.textContent = item.item
             li.append(label)
@@ -59,7 +60,7 @@ export default class ListTemplate implements DOMList  {
 
             // create button element for delete and also append to the parent li 
             const button = document.createElement("button") as HTMLButtonElement
-            button.className = "button size-[26px] rounded-md p-1 hover:bg-red-50 hover:text-red-500 block peer-has-[:checked]:hidden"
+            button.className = "button my-auto bg-indigo-600 size-[26px] rounded-md ml-auto w-fit px-[1rem] py-[0] bg-[#4158D0] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 block peer-has-[:checked]:hidden"
             button.textContent = "X"
             li.append(button)
 
